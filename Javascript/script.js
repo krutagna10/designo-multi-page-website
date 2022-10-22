da6e002ec228a4316576b18289a82b136def0403
Krutagna Patel <kpatel.kp7016@gmail.com>
2022-10-22 23:30:53 +0530

    Initial commit

---
+++ Javascript/script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const mainHeader = document.querySelector('.main-header')
+const overlay = document.querySelector('.overlay');
+const webDesignProjects = document.querySelectorAll('.web-design');
+const appDesignProjects = document.querySelectorAll('.app-design');
+const graphicDesignProjects = document.querySelectorAll('.graphic-design');
+
+// Navigation
+let mobileNavigationButton = document.querySelector('.mobile-navigation-button');
+
+mobileNavigationButton.addEventListener('click', () => {
+    mainHeader.classList.toggle('navigation-open');
+    mobileNavigationButton.classList.toggle('active');
+    overlay.classList.toggle('hidden');
+});
+
+// Removing overlay
+const hideMobileNavigation = () => {
+    overlay.classList.add('hidden');
+    mainHeader.classList.remove('navigation-open');
+    mobileNavigationButton.classList.remove('active')
+}
+
+// When the user click on escape key
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        hideMobileNavigation();
+    }
+})
+
+// When the width is greater than 768 pixels
+window.addEventListener("resize", function() {
+    if (window.innerWidth > 768) {
+        hideMobileNavigation()
+    }
+});
+
+
+
+// When the user clicks on any project container
+for (const webDesignProject of webDesignProjects) {
+    webDesignProject.addEventListener('click', () => {
+        window.location.href = 'web-design.html';
+    });
+}
+
+for (const appDesignProject of appDesignProjects) {
+    appDesignProject.addEventListener('click', () => {
+        window.location.href = 'app-design.html';
+    });
+}
+
+for (const graphicDesignProject of graphicDesignProjects) {
+    graphicDesignProject.addEventListener('click', () => {
+        window.location.href = 'graphic-design.html';
+    });
+}
+
+
+
---
+++ README.md
@@ -0,0 +1,10 @@
+# Frontend Mentor - Designo Multi Page Website
+
+### Screenshot
+
+![](Screenshot/Screenshot.png)
+
+### Links
+
+- Live Site URL: [https://designo-multi-page-website-10.vercel.app/]
+
